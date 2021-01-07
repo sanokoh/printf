517521ae2a897686d1e429f231fd88db747af95c
sanokoh <sanokoh@outlook.com>
2021-01-07 11:48:51 +0900

    20210107

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ksano <ksano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 12:10:13 by ksano             #+#    #+#             */
-/*   Updated: 2021/01/07 11:43:31 by ksano            ###   ########.fr       */
+/*   Updated: 2021/01/07 11:48:02 by ksano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,36 +44,3 @@ int				ft_printf(const char *format, ...)
 	va_end(ap);
 	return (res);
 }
-
-#ifdef TEST
-# ifdef FT_PRINTF
-#  define F(...) \
-    res = ft_printf(__VA_ARGS__); \
-    printf("%d\n", res);
-# else
-#  define F(...) \
-    res = printf(__VA_ARGS__); \
-	printf("%d\n", res);
-# endif
-int		main(void)
-{
-	setvbuf(stdout, 0, _IONBF, 0);
-	int res;
-	int t;
-	char c;
-
-	c = 'a';
-	t = 50;
-
-	// F("1unsigned 1 %u unsigned 2 %u\n\n", 42, -42);
-	// F("3unsigned 1 %   u unsigned 2 % u\n\n", 42, -42);
-	F("4unsigned 1 %-12u unsigned 2 %-12u\n\n", 42, -42);
-	F("6unsigned 1 %012u unsigned 2 %012u\n\n", 42, -42);
-
-	// F("%d\n\n", F("1unsigned 1 %u unsigned 2 %u\n\n", 42, -42));
-	// F("%d\n\n", F("3unsigned 1 %   u unsigned 2 % u\n\n", 42, -42));
-	// F("%d\n\n", F("4unsigned 1 %-12u unsigned 2 %-12u\n\n", 42, -42));
-	// F("%d\n\n", F("6unsigned 1 %012u unsigned 2 %012u\n\n", 42, -42));
-
-}
-#endif
