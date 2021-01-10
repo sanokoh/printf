72ec60a29af778bd60997bb504c639b3e4ec5401
sanokoh <sanokoh@outlook.com>
2021-01-10 17:54:57 +0900

    20210110

--- test.sh
+++
@@ -1,8 +0,0 @@
-gcc -Wno-format -D TEST -o printf main.c libftprintf.a
-gcc -Wno-format -g -D TEST -D FT_PRINTF -o ft_printf main.c libftprintf.a
-#make ft_printf
-#make printf
-./ft_printf > ft_printf.txt
-./printf > printf.txt
-diff -a --suppress-common-lines ft_printf.txt printf.txt
-vimdiff printf.txt ft_printf.txt
